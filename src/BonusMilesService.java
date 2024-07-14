de4b70a361957391306d6fd6c467bedc646a8835
Pavel MSK <sonev@bk.ru>
2024-07-14 16:09:09 +0300

    commit bonus

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,9 @@
 public class BonusMilesService {
+
+    public int calculate(int ticketPrice) {
+        int milesCoefficient = 20; // Коэффициент для расчета количества миль
+        int miles = ticketPrice / milesCoefficient; // Расчет количества миль
+        return miles;
+    }
+
 }
